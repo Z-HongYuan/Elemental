da8895e60a50e189f07bb87558f5af6c5b7c83bc
Z-HongYuan <1939218189@qq.com>
2026-02-10 18:47:22 +0800

    添加用于处理动态角色脸部阴影的组件

---
+++ Source/Elemental/Components/ElemFaceShadowComp.cpp
@@ -0,0 +1,73 @@
+﻿// Copyright © 2026 鸿源z. All Rights Reserved.
+
+
+#include "ElemFaceShadowComp.h"
+
+
+UElemFaceShadowComp::UElemFaceShadowComp() :
+	FaceForwardValueName("SDF_F"),
+	FaceRightValueName("SDF_R"),
+	FaceForwardBoneName("SDF_F"),
+	FaceRightBoneName("SDF_R"),
+	HeadBoneName("Head"),
+	SkeletalMeshComponentTag("UseFaceShadow")
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	SetIsReplicated(false);
+}
+
+
+void UElemFaceShadowComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//TODO 修改面部曲线状态,在光源在面部侧面时,过渡太快
+	if (FaceMaterialInstance and ParentMeshComponent.IsValid())
+	{
+		const FVector Forward = ParentMeshComponent->GetSocketLocation(FaceForwardBoneName);
+		const FVector Right = ParentMeshComponent->GetSocketLocation(FaceRightBoneName);
+		const FVector Head = ParentMeshComponent->GetSocketLocation(HeadBoneName);
+
+		FaceMaterialInstance->SetVectorParameterValue(FaceForwardValueName, (Forward - Head).GetSafeNormal());
+		FaceMaterialInstance->SetVectorParameterValue(FaceRightValueName, (Right - Head).GetSafeNormal());
+	}
+}
+
+
+void UElemFaceShadowComp::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RefreshFaceMaterialInstance();
+}
+
+void UElemFaceShadowComp::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (FaceMaterialInstance != nullptr)
+	{
+		FaceMaterialInstance = nullptr;
+		UE_LOG(LogTemp, Log, TEXT("UElemFaceShadowComp::EndPlay() FaceMaterialInstance is Set nullptr"));
+	}
+	ParentMeshComponent = nullptr;
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void UElemFaceShadowComp::RefreshFaceMaterialInstance()
+{
+	if (FaceMaterialInstance != nullptr)
+	{
+		FaceMaterialInstance = nullptr;
+		UE_LOG(LogTemp, Log, TEXT("UElemFaceShadowComp::RefreshFaceMaterialInstance() FaceMaterialInstance is Set nullptr"));
+	}
+
+	ParentMeshComponent = Cast<USkeletalMeshComponent>(GetOwner()->FindComponentByTag(USkeletalMeshComponent::StaticClass(), SkeletalMeshComponentTag));
+
+	if (ParentMeshComponent.IsValid())
+	{
+		//父类没有材质可供创建动态的时候,会崩溃
+		FaceMaterialInstance = ParentMeshComponent->CreateDynamicMaterialInstance(FaceID, ParentMeshComponent->GetMaterial(FaceID), "FaceShadowMI");
+		//log
+		UE_LOG(LogTemp, Log, TEXT("UElemFaceShadowComp::RefreshFaceMaterialInstance() FaceMaterialInstance is Created"));
+	}
+}
---
+++ Source/Elemental/Components/ElemFaceShadowComp.h
@@ -0,0 +1,69 @@
+﻿// Copyright © 2026 鸿源z. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ElemFaceShadowComp.generated.h"
+
+/*
+ * 控制角色脸部SDF阴影的插件
+ * 通过获取网格体中的位置信息,设置材质参数值
+ * 需要设置全部的FName参数
+ * 将在BeginPlay()中调用一次RefreshFaceMaterialInstance(),如果角色更换了模型,请调用此函数以刷新材质实例
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ELEMENTAL_API UElemFaceShadowComp : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UElemFaceShadowComp();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY(Transient)
+	TWeakObjectPtr<USkeletalMeshComponent> ParentMeshComponent;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> FaceMaterialInstance;
+
+private:
+
+public:
+	//脸部材质ID
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	int FaceID;
+
+	//需要变化的材质属性名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceForwardValueName;
+
+	//需要变化的材质属性名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceRightValueName;
+
+	//骨骼插槽名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceForwardBoneName;
+
+	//骨骼插槽名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceRightBoneName;
+
+	//骨骼插槽名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName HeadBoneName;
+
+	//骨骼网格体组件拥有的Tag
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName SkeletalMeshComponentTag;
+
+	//刷新面部材质实例,在更换角色的时候非常有用
+	UFUNCTION(BlueprintCallable, Category = "SDF")
+	void RefreshFaceMaterialInstance();
+};
