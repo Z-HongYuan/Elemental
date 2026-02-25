28d0f3f69f1cbcd9268f7a65f6f17fd0e514f70a
Z-HongYuan <1939218189@qq.com>
2026-02-25 15:37:08 +0800

    添加带有NameSpace的基础Tag集合

---
+++ Source/Elemental/Data/ElementalTags/ElemGameplayTags.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright © 2026 鸿源z. All Rights Reserved.
+
+
+#include "ElemGameplayTags.h"
+
+namespace ElementalTags
+{
+	// ELEMENTAL_API UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Elem_Test, "Elem.Test", "注释Test");
+}
---
+++ Source/Elemental/Data/ElementalTags/ElemGameplayTags.h
@@ -0,0 +1,11 @@
+﻿// Copyright © 2026 鸿源z. All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+
+namespace ElementalTags
+{
+	// ELEMENTAL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Elem_Test);
+}
