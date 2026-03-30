5fb0b6e5f541e30b79de353cd0c28e6f033dc166
Z-HongYuan <1939218189@qq.com>
2026-03-30 13:24:18 +0800

    格式化代码,添加CommonUI插件引用

--- Plugins/OwnCustomPlugins/ModularGame/Source/ModularGame/ModularGame.Build.cs
+++ Plugins/OwnCustomPlugins/ModularGame/Source/ModularGame/ModularGame.Build.cs
@@ -7,30 +7,33 @@ public class ModularGame : ModuleRules
 	public ModularGame(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
 		PublicIncludePaths.AddRange(
-			new string[] {
+			new string[]
+			{
 				// ... add public include paths required here ...
 			}
-			);
-				
-		
+		);
+
+
 		PrivateIncludePaths.AddRange(
-			new string[] {
+			new string[]
+			{
 				// ... add other private include paths required here ...
 			}
-			);
-			
-		
+		);
+
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
+				"CommonUI"
 				// ... add other public dependencies that you statically link with here ...
 			}
-			);
-			
-		
+		);
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -40,14 +43,14 @@ public class ModularGame : ModuleRules
 				"SlateCore",
 				// ... add private dependencies that you statically link with here ...	
 			}
-			);
-		
-		
+		);
+
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
 				// ... add any modules that your module loads dynamically here ...
 			}
-			);
+		);
 	}
 }
