9721894f63e1f7627a2d6449f56368ff36e1833e
Z-HongYuan <1939218189@qq.com>
2026-04-02 18:48:45 +0800

    feat(subtitles): 添加模块化游戏字幕功能
    - 新增 ModularGameSubtitles 模块框架
    - 实现模块启动和关闭功能
    - 配置模块依赖项包括 Core、CoreUObject、Engine、Slate 和 SlateCore
    - 在插件配置文件中注册字幕模块
    - 设置模块加载阶段为默认阶段

---
+++ Plugins/OwnFramework/Source/ModularGameSubtitles/Private/ModularGameSubtitles.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FModularGameSubtitlesModule"
+
+class FModularGameSubtitlesModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FModularGameSubtitlesModule::StartupModule()
+{
+}
+
+void FModularGameSubtitlesModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FModularGameSubtitlesModule, ModularGameSubtitles)
