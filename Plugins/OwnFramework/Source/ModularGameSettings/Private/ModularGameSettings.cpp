4274aef90266db56411bb78b47bd035f13745359
Z-HongYuan <1939218189@qq.com>
2026-04-02 18:46:01 +0800

    feat(ModularGameSettings): 添加模块化游戏设置功能
    - 创建了 ModularGameSettings 模块的基本框架
    - 实现了 FModularGameSettingsModule 类及其启动和关闭方法
    - 配置了模块的构建依赖关系，包括 Core、CoreUObject、Engine、Slate 和 SlateCore
    - 在 OwnFramework.uplugin 文件中注册了新的 ModularGameSettings 模块
    - 设置模块类型为 Runtime，加载阶段为 Default

---
+++ Plugins/OwnFramework/Source/ModularGameSettings/Private/ModularGameSettings.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FModularGameSettingsModule"
+
+class FModularGameSettingsModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FModularGameSettingsModule::StartupModule()
+{
+}
+
+void FModularGameSettingsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FModularGameSettingsModule, ModularGameSettings)
