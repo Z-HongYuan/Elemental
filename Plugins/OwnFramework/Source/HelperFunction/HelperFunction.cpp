806275d8708160b950054268fcdce45e5e5368fb
Z-HongYuan <1939218189@qq.com>
2026-04-14 18:28:22 +0800

    添加HelperFunction模块

---
+++ Plugins/OwnFramework/Source/HelperFunction/HelperFunction.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FHelperFunctionModule"
+
+class FHelperFunctionModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FHelperFunctionModule::StartupModule()
+{
+}
+
+void FHelperFunctionModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FHelperFunctionModule, HelperFunction)
+
+/*
+ * 融合了Lyra中AsyncMixin插件的模块
+ * 还添加了其他在项目中使用的方便函数
+ */
